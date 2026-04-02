9ff83bce4ddefec78e45b335958ca80045dc7ffe
zonov <140524793+jesusdangerous@users.noreply.github.com>
2026-04-02 13:23:20 +0500

    Update filesystems.nix

--- modules/nixos/filesystems.nix
+++ modules/nixos/filesystems.nix
@@ -3,15 +3,22 @@
 
 {
   fileSystems = {
-    "/mnt/backups" = {
-      device = "/dev/disk/by-uuid/55287544-ce9f-4c93-a2f6-a63b69623fe1";
+    "/" = {
+      device = "/dev/disk/by-uuid/8311b19e-5fe7-4b4f-8196-9c5f7b9bca8";
       fsType = "ext4";
-      options = [ "defaults" "noatime" ]; #"uid=1000" "gid=1000" "dmask=007" "fmask=117" ];
+      options = [ "defaults" "noatime" ];
     };
-    "/mnt/shit" = {
-      device = "/dev/disk/by-uuid/234dc4ed-ade3-447f-af5a-4d254835cc66";
+
+    "/nix" = {
+      device = "/dev/disk/by-uuid/8B8C-CABC";
+      fsType = "ext4";  
+      options = [ "defaults" "noatime" ];
+    };
+
+    "/mnt/data" = {
+      device = "/dev/disk/by-uuid/a3ced3c3-320d-42e9-9cbc-d957c74f7a0";
       fsType = "ext4";
-      options = [ "defaults" "noatime" ]; #"uid=1000" "gid=1000" "dmask=007" "fmask=117" ];
+      options = [ "defaults" "noatime" ];
     };
   };
 }
